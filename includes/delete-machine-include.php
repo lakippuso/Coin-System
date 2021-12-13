2f8f477cc16700c5b3703770912b7bcea5218eb9
lakippuso <84003904+lakippuso@users.noreply.github.com>
2021-12-13 20:32:41 +0800

    delete machine

--- includes/delete-machine-include.php
+++ includes/delete-machine-include.php
@@ -2,7 +2,7 @@
     session_start();
     if(!empty($_SESSION['session_id'])){
         include 'config.php';
-        $id = $_GET['machine_id'];
+        $id = $_POST['machine_id'];
         $sql = "DELETE FROM machine_info WHERE machine_id = '$id'";
         mysqli_query($con,$sql);
     }
