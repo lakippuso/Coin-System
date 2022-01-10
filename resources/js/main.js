c2205119218df3ed913e7c2e17e9da15cd7c0549
lakippuso <84003904+lakippuso@users.noreply.github.com>
2022-01-10 14:14:52 +0800

    add income data in machine page

--- resources/js/main.js
+++ resources/js/main.js
@@ -32,6 +32,9 @@ function configuration(id){
         $('#machine-config-id').val(data['id']);
         $('#machine-config-name').val(data['name']);
         $('#machine-config-type').val(data['type']);
+        $('#overall-income').val(data['total']);
+        $('#detail-daily-income').val(data['daily']);
+        $('#detail-monthly-income').val(data['monthly']);
       });
 }
 //Chart in Dashboard
